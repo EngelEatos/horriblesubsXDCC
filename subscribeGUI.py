c7fa5ade71ccec19f2124670dc6a945d746f7e52
EngelEatos <engel-eatos@web.de>
2017-07-23 04:56:51 +0200

    udpated

--- subscribeGUI.py
+++ subscribeGUI.py
@@ -1,5 +1,10 @@
+import os
+import json
 import tkinter as tk
+import config
 
+CONFIG_FILE = config.CONFIG_FILE
+checkVar = dict()
 animes = ["Shoukoku no Altair", "Netsuzou TRap - NTR", "18if", "Knight's & Magic",
 "Nana Maru San Batsu", "Saiyuki Reload Blast", "Isekai Shokudou", "Keppeki Danshi! Aoyama-kun",
 "Isekai wa Smartphone to Tomo ni.", "Katsugeki Touken Ranbu", "Enmusubi no Youko-chan",
@@ -7,22 +12,29 @@ animes = ["Shoukoku no Altair", "Netsuzou TRap - NTR", "18if", "Knight's & Magic
 "Ballroom e Youkoso", "Nora to Oujo no Noraneko Heart", "Sin - Nanatsu no Taizai", "Monster Strike 2",
 "Hajimete no Gal", "Boku no Hero Academia", "Sagrada Reset", "Youkoso Jitsuryoku Shijou Shugi no Kyoushitsu e", "Aho Girl"]
 
+def load():
+    with open(CONFIG_FILE, 'r') as config:
+        return json.load(config)
+
 def save():
     animes = checkVar.keys()
+    data = []
     for anime in animes:
-        value = checkVar.get(anime).get()
-        if value == 1:
-            data.append(anime)
-    print(data)
+        if checkVar.get(anime).get() == 1:
+            result.append(anime)
+    if os.path.isfile(CONFIG_FILE):
+        os.remove(CONFIG_FILE)
+    with open(CONFIG_FILE, 'w', encoding='utf-8') as outfile:
+        json.dump(data, outfile, ensure_ascii=False)
 
 class frame(tk.Frame):
-    def __init__(self, root, *args, **kwargs):
+    def __init__(self, root, data, animes, *args, **kwargs):
         tk.Frame.__init__(self, root, *args, **kwargs)
         self.root = root
-
+        self.data = data
+        self.animes = animes
         self.vsb = tk.Scrollbar(self, orient="vertical")
-        self.text = tk.Text(self, width=40, height=20,
-                            yscrollcommand=self.vsb.set)
+        self.text = tk.Text(self, width=40, height=20, yscrollcommand=self.vsb.set)
         self.vsb.config(command=self.text.yview)
         self.vsb.pack(side="right", fill="y")
         self.text.pack(side="left", fill="both", expand=True)
@@ -30,14 +42,23 @@ class frame(tk.Frame):
         for idx, anime in enumerate(animes):
             checkVar[anime] = tk.IntVar()
             c = tk.Checkbutton(self, text=anime, variable=checkVar[anime])
+            c.config(bg="white")
+            if anime in self.data:
+                c.toggle()
             self.text.window_create("end", window=c)
             self.text.insert("end", "\n")
         self.btn = tk.Button(text='save', command=lambda: save())
         self.btn.pack(side="bottom", fill="both", expand=True)
+        self.text.config(state="disabled")
 
+def create_gui(animes):
+    data = load()
+    root = tk.Tk()
+    frame(root, data, animes).pack(side="top", fill="both", expand=True)
+    root.mainloop()
 
-checkVar = dict()
-data = []
-root = tk.Tk()
-frame(root).pack(side="top", fill="both", expand=True)
-root.mainloop()
+def main():
+    create_gui(animes)
+
+if __name__ == '__main__':
+    main()
